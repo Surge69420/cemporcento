77b061894fc63af78d4fb962f2a2d0f780a38f8b
Zain ul abdien <74106328+Surge69420@users.noreply.github.com>
2025-03-07 16:15:48 +0000

    i forgot to commit again

--- index.js
+++ index.js
@@ -1,165 +1,194 @@
-
-document.getElementById("MenuBtn").addEventListener("click", ()=> {document.getElementById("MenuBar").classList.toggle("active");
+document.getElementById("MenuBtn").addEventListener("click", () => {
+  document.getElementById("MenuBar").classList.toggle("active");
+  let displayTogglers = document.querySelectorAll(".DisplayToggler");
+  let displayTogglersarr = [...displayTogglers];
+  displayTogglersarr.forEach(element => {
+    let data = element.getAttribute("data-target");
+    togglerList = document.querySelectorAll(".Toggleable");
+    togglerArr = [...togglerList];
+    togglerArr.forEach(e => {
+      e.classList.remove("active");
+    })
+  });
 });
+
 let displayTogglers = document.querySelectorAll(".DisplayToggler");
 let displayTogglersarr = [...displayTogglers];
 displayTogglersarr.forEach(element => {
-    let data = element.getAttribute("data-target");
-    element.addEventListener("click",()=> {
-       togglerList =document.querySelectorAll(".Toggleable");
-       togglerArr = [...togglerList];
-       togglerArr.forEach(e => {
-        if(e.getAttribute("data-id") === data)
-        {
-            e.style.display = "block";
-        }else {
-            e.style.display = "none";
-        }
-       })
+  let data = element.getAttribute("data-target");
+  element.addEventListener("click", () => {
+    togglerList = document.querySelectorAll(".Toggleable");
+    togglerArr = [...togglerList];
+    togglerArr.forEach(e => {
+      if (e.getAttribute("data-id") === data) {
+        e.classList.toggle("active");
+      } else {
+        e.classList.remove("active");
+      }
     })
+  })
 });
+
+
 window.onscroll = function () {
-    let topbar = document.querySelector("nav");
-    if(window.scrollY > 50){
-        topbar.classList.add("Scrolled");
-    }else {
-        topbar.classList.remove("Scrolled");
-    }
-}
-$('.multiple').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    customPaging: function(slider, i) {
-        // this example would render "tabs" with titles
-        return '<div class="dot"></div>';
-      },
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 762,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-$('.GridSlider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    customPaging: function(slider, i) {
-        // this example would render "tabs" with titles
-        return '<div class="dot"></div>';
-      },
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 762,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
-  function DropDownSelect(e)
-  {
-    closestDropDown = e.closest(".dropdown");
-    closestDropDown.firstElementChild.textContent = e.textContent;
+  let topbar = document.querySelector("nav");
+  if (window.scrollY > 50) {
+    topbar.classList.add("Scrolled");
+  } else {
+    topbar.classList.remove("Scrolled");
   }
+}
 
-  $('.cemPorcentoSlider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    customPaging: function(slider, i) {
-        // this example would render "tabs" with titles
-        return '<div class="dot"></div>';
-      },
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 762,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+
+$('.multiple').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  customPaging: function (slider, i) {
+    // this example would render "tabs" with titles
+    return '<div class="dot"></div>';
+  },
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 762,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
+
+$('.GridSlider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  customPaging: function (slider, i) {
+    // this example would render "tabs" with titles
+    return '<div class="dot"></div>';
+  },
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 762,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+function DropDownSelect(e) {
+  closestDropDown = e.closest(".dropdown");
+  closestDropDown.firstElementChild.textContent = e.textContent;
+}
+
+$('.cemPorcentoSlider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  customPaging: function (slider, i) {
+    // this example would render "tabs" with titles
+    return '<div class="dot"></div>';
+  },
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 762,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+let ProductsPageRedirect = document.querySelectorAll(".ProductsPageRedirect");
+ProductsPageRedirect.forEach((element) => {
+  element.addEventListener("click", () => { window.location.href = "./Products.html" })
+})
+
+
+let gridItem = document.querySelectorAll(".griditem");
+let cemPorcentoGridChildren = document.querySelector(".cemPorcentoGrid").children;
+let Items = document.querySelectorAll(".item");
+let itemsArr = [...gridItem, ...Items, ...cemPorcentoGridChildren];
+
+itemsArr.forEach((element) => {
+  element.addEventListener("click", () => { window.location.href = "./Product.html" })
+})
